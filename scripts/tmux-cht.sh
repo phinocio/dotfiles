5aaf90f585fefeee87607e9b9028077b3b3110d4
Phinocio <github@phinocio.com>
2023-11-05 22:37:17 -0700

    update

--- scripts/tmux-cht.sh
+++ scripts/tmux-cht.sh
@@ -9,7 +9,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" $XDG_CONFIG_HOME/tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | bat"
+	tmux neww bash -c "curl -s cht.sh/$selected/$query | bat --paging=always"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | bat"
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | bat --paging=always"
 fi
