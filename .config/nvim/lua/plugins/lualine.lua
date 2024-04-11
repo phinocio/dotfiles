a8bd662335d068b9ea3bd929ee83b9de3c182fc5
Phinocio <github@phinocio.com>
2024-04-10 23:45:06 -0600

    feat: add lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,52 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+
+	opts = function()
+		local lazy_status = require("lazy.status")
+		return {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = true,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					"branch",
+					"diff",
+					'"🖧  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+					{ "diagnostics", sources = { "nvim_diagnostic" } },
+				},
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = {
+					{ lazy_status.updates, cond = lazy_status.has_updates },
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = {
+					'(vim.bo.expandtab and "␠ " or "⇥ ") .. " " .. vim.bo.shiftwidth',
+				},
+				lualine_z = {
+					"location",
+					"progress",
+				},
+			},
+		}
+	end,
+}
