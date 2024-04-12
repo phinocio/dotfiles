d6e61f21798ce010d9a92426c176f5712ef1ecae
Phinocio <github@phinocio.com>
2024-04-11 22:30:10 -0600

    feat: remove snippets
    A lot in these plugins are kinda pointless...Imma make my own

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -7,8 +7,8 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
-		"rafamadriz/friendly-snippets",
-		"saadparwaiz1/cmp_luasnip",
+		--		"rafamadriz/friendly-snippets",
+		--		"saadparwaiz1/cmp_luasnip",
 	},
 	config = function()
 		local cmp = require("cmp")
