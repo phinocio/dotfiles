fd54f23a9e23724fd68d2e935b8116a3b59e4416
Phinocio <github@phinocio.com>
2024-04-12 20:15:41 -0600

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,14 +6,14 @@ local config = wezterm.config_builder()
 
 -- Font
 config.font = wezterm.font_with_fallback({
-	"Hack",
-	"Cascadia Code",
-	"JetBrains Mono",
 	{
 		family = "Monaspace Radon",
 		weight = "Medium",
 		harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
 	},
+	"Hack",
+	"Cascadia Code",
+	"JetBrains Mono",
 	"Symbols Nerd Font Mono",
 })
 config.font_size = 12
