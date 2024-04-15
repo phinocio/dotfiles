710718aa731f102a93368b3a88f0c35b465fb856
Phinocio <github@phinocio.com>
2024-04-14 23:11:07 -0600

    change padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -38,10 +38,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = true
 config.window_decorations = "RESIZE"
 config.window_padding = {
-	left = 8,
-	right = 8,
-	top = 4,
-	bottom = 4,
+	left = 12,
+	right = 12,
+	top = 12,
+	bottom = 12,
 }
 
 -- Cursor blinks too slow for me
