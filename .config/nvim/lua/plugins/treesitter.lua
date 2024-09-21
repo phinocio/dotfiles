35af0a614f2148d691bc8bfa4fd1fe7a6dd5c684
Phinocio <github@phinocio.com>
2024-09-20 21:19:01 -0600

    feat(nvim): add blade treesitter highlightings

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,43 @@
+return {
+
+	-- #########################################
+	-- Disable treesitter related plugins
+	-- #########################################
+
+	-- #########################################
+	-- Change config of existing LazyVim treesitter plugins
+	-- #########################################
+	{
+		-- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"blade",
+				"php_only",
+			})
+		end,
+		config = function(_, opts)
+			vim.filetype.add({
+				pattern = {
+					[".*%.blade%.php"] = "blade",
+				},
+			})
+
+			require("nvim-treesitter.configs").setup(opts)
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			---@diagnostic disable-next-line: inject-field
+			parser_config.blade = {
+				install_info = {
+					url = "https://github.com/EmranMR/tree-sitter-blade",
+					files = { "src/parser.c" },
+					branch = "main",
+				},
+				filetype = "blade",
+			}
+		end,
+	},
+
+	-- #########################################
+	-- Add new treesitter plugins
+	-- #########################################
+}
