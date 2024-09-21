ac4cc8ecbe84fca385b69d82f566957b92935e7c
Phinocio <github@phinocio.com>
2024-09-20 23:37:29 -0600

    feat(nvim): use neocodeium

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -40,4 +40,28 @@ return {
 		},
 		config = true,
 	},
+	{
+		"monkoose/neocodeium",
+		event = "VeryLazy",
+		config = function()
+			local neocodeium = require("neocodeium")
+			neocodeium.setup()
+			vim.keymap.set("i", "<A-y>", neocodeium.accept)
+			vim.keymap.set("i", "<A-w>", function()
+				require("neocodeium").accept_word()
+			end)
+			vim.keymap.set("i", "<A-a>", function()
+				require("neocodeium").accept_line()
+			end)
+			vim.keymap.set("i", "<A-p>", function()
+				require("neocodeium").cycle_or_complete()
+			end)
+			vim.keymap.set("i", "<A-n>", function()
+				require("neocodeium").cycle_or_complete(-1)
+			end)
+			vim.keymap.set("i", "<A-c>", function()
+				require("neocodeium").clear()
+			end)
+		end,
+	},
 }
