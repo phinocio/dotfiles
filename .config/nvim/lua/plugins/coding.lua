80c1a5eabe823335ea864489336cbe008a317886
Phinocio <github@phinocio.com>
2024-09-22 14:26:49 -0600

    feat(nvim): update keymaps for cmp and telescope

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -6,6 +6,16 @@ return {
 	-- #########################################
 	-- Change config of existing LazyVim coding plugins
 	-- #########################################
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+			})
+		end,
+	},
 
 	-- #########################################
 	-- Add new coding plugins
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -13,13 +13,14 @@ return {
 	},
 	{
 		"nvim-telescope/telescope.nvim",
-		keys = {
-			{
-				"<leader>fp",
-				function()
-					require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-				end,
-				desc = "Find Plugin File",
+		opts = {
+			defaults = {
+				mappings = {
+					i = {
+						["<C-j>"] = "move_selection_next",
+						["<C-k>"] = "move_selection_previous",
+					},
+				},
 			},
 		},
 	},
