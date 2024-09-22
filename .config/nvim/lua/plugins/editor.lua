8f184db9d9d985f9e25d4907585527b51bb78c17
Phinocio <github@phinocio.com>
2024-09-22 11:39:00 -0600

    feat(nvim): add vim-tmux-navigator

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -48,4 +48,21 @@ return {
 			{ "<leader>e", "<cmd>Oil<CR>", desc = "Open oil file explorer" },
 		},
 	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>" },
+			{ "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>" },
+			{ "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>" },
+			{ "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>" },
+			{ "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>" },
+		},
+	},
 }
