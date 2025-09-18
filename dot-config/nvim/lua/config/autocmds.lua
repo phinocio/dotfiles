e0868cd082e7f4aa0a9d73efea882aefb5a59c56
Phinocio <github@phinocio.com>
2025-09-17 20:29:10 -0600

    feat(nvim): initial lazyvim redo

--- dot-config/nvim/lua/config/autocmds.lua
+++ dot-config/nvim/lua/config/autocmds.lua
@@ -6,7 +6,3 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-
--- Stops newline continuation of comments
-vim.cmd("autocmd BufEnter * set formatoptions-=cro")
-vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
