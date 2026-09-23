a6ce0ac308c8ec08047f82ca90e55f985511d80f
Phinocio <github@phinocio.com>
2026-09-23 13:04:15 -0600

    feat(superfile): update config

--- dot-config/zsh/functions.zsh
+++ dot-config/zsh/functions.zsh
@@ -9,3 +9,24 @@ chpwd() {
 		fi
 	fi
 }
+
+spf() {
+    os=$(uname -s)
+
+    # Linux
+    if [[ "$os" == "Linux" ]]; then
+        export SPF_LAST_DIR="${XDG_STATE_HOME:-$HOME/.local/state}/superfile/lastdir"
+    fi
+
+    # macOS
+    if [[ "$os" == "Darwin" ]]; then
+        export SPF_LAST_DIR="$HOME/Library/Application Support/superfile/lastdir"
+    fi
+
+    command spf "$@"
+
+    [ ! -f "$SPF_LAST_DIR" ] || {
+        . "$SPF_LAST_DIR"
+        rm -f -- "$SPF_LAST_DIR" > /dev/null
+    }
+}
